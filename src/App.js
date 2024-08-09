03b6c53fff7d85379af8615097ce51d2eb4709e4
Reza Br <reza.linusx@gmail.com>
2024-08-09 22:29:08 +0330

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
+import TaskProvider from "./context/TaskContext";
+import { useTask } from "./context/TaskContext";
+
+import Form from "./component/Form";
+import TaskList from "./component/TaskList";
+
 function App() {
-  return <div className="App"></div>;
+  const tasks = useTask();
+  console.log(tasks);
+
+  return (
+    <div className="App">
+      <Form />
+      <TaskList tasks={tasks} />
+    </div>
+  );
 }
 
 export default App;
