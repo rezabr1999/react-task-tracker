2a66a783aed720a401513da251c0a141bea60f06
Reza Br <reza.linusx@gmail.com>
2024-08-09 22:27:39 +0330

    implement task and tasklist components

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,7 +1,40 @@
-export default function Task() {
+import { useState } from "react";
+
+export default function Task({
+  task,
+  handleDelete,
+  handleUpdate,
+  handleCheck,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedText, setUpdatedText] = useState(task.text);
+
+  const onUpdateClick = () => {
+    handleUpdate({ ...task, text: updatedText });
+    setIsEditing(false);
+  };
+
   return (
     <div>
-      <h1>Task</h1>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            value={updatedText}
+            onChange={(e) => setUpdatedText(e.target.value)}
+          />
+          <button onClick={onUpdateClick}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <p>{task.text}</p>
+          <p onClick={() => handleCheck(task)}>
+            {task.done ? "Done" : "Not Done"}
+          </p>
+          <button onClick={() => handleDelete(task)}>Delete</button>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+        </div>
+      )}
     </div>
   );
 }
--- src/component/TaskList.jsx
+++ src/component/TaskList.jsx
@@ -1,7 +1,32 @@
+import { useTaskDispatch } from "../context/TaskContext";
+import Task from "./Task";
+
 export default function TaskList({ tasks }) {
+  const dispatch = useTaskDispatch();
+
+  const handleDelete = (task) => {
+    dispatch({ type: "delete", task: task });
+  };
+
+  const handleUpdate = (updatedTask) => {
+    dispatch({ type: "update", task: updatedTask });
+  };
+
+  const handleCheck = (task) => {
+    dispatch({ type: "check", task: task });
+  };
+
   return (
     <div>
-      <h1>TaskList</h1>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          handleDelete={handleDelete}
+          handleUpdate={handleUpdate}
+          handleCheck={handleCheck}
+        />
+      ))}
     </div>
   );
 }
