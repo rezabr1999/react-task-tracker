f37c17872130cd6db068c6d5d8f257ac6576a68e
Reza Br <reza.linusx@gmail.com>
2024-08-10 16:59:03 +0330

    adjust UI for mobile first design

--- src/component/TaskUI.jsx
+++ src/component/TaskUI.jsx
@@ -5,14 +5,14 @@ export default function TaskUI({
   handleEdit,
 }) {
   const doneClass = task.done
-    ? "px-4 py-2 bg-green-500 rounded-md"
-    : "px-4 py-2  bg-red-500 rounded-md";
+    ? "px-4 py-2 bg-green-500 rounded-md flex-1"
+    : "px-4 py-2  bg-red-500 rounded-md flex-1";
 
   return (
-    <div className="flex justify-between items-center w-full flex-1 p-5">
-      <p>{task.text}</p>
+    <div className="flex justify-between flex-col gap-4 items-center w-full flex-1 p-5">
+      <p className="w-full p-4">{task.text}</p>
 
-      <div className="flex gap-3">
+      <div className="flex gap-3 w-full">
         <button
           className={doneClass}
           onClick={() => handleCheck(task)}
@@ -20,13 +20,13 @@ export default function TaskUI({
           {task.done ? "Done" : "Not Done"}
         </button>
         <button
-          className="bg-red-600 px-4 py-2 rounded-md text-white"
+          className="bg-red-600 px-4 py-2 rounded-md text-white flex-1"
           onClick={() => handleDelete(task)}
         >
           Delete
         </button>
         <button
-          className="bg-orange-400 px-4 py-2 rounded-md text-white"
+          className="bg-orange-400 px-4 py-2 rounded-md text-white flex-1 "
           onClick={() => handleEdit(true)}
         >
           Edit
