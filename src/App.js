3aa508f6f7a85766c3bb150f674544d1d0e1d432
Reza Br <reza.linusx@gmail.com>
2024-08-13 21:44:50 +0330

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import "./App.css";
-import TaskProvider from "./context/TaskContext";
 import { useTask } from "./context/TaskContext";
-
 import { useTheme } from "./context/ThemeContext";
-import { useThemeDispatch } from "./context/ThemeContext";
 
 import Form from "./component/Form";
 import TaskList from "./component/TaskList";
-
 import ThemeToggleButton from "./component/ThemeToggleButton";
-
-import { IoMdSunny } from "react-icons/io";
-import { MdNightsStay } from "react-icons/md";
 
 function App() {
   const theme = useTheme();
-  const themeDispatch = useThemeDispatch();
   const tasks = useTask();
 
-  const changeTheme = () => {
-    themeDispatch({ type: theme === "light" ? "dark" : "light" });
-  };
-
   return (
-    // Apply the dark class based on the theme
-    <div className={`${theme === "dark" ? "dark" : ""}`}>
-      <div className="p-5 bg-slate-100 dark:bg-slate-800 min-h-screen">
-        <div className="flex flex-col gap-10 max-w-3xl mx-auto">
-          <ThemeToggleButton
-            icon={theme === "light" ? <IoMdSunny /> : <MdNightsStay />}
-          />
-
-          <Form />
-          <TaskList tasks={tasks} />
-        </div>
+    <div className={theme === "dark" ? "dark" : ""}>
+      <div className="p-5 bg-slate-100 dark:bg-slate-800 min-h-screen flex flex-col gap-10 max-w-3xl mx-auto">
+        <ThemeToggleButton />
+        <Form />
+        <TaskList tasks={tasks} />
       </div>
     </div>
   );
--- src/component/ThemeToggleButton.jsx
+++ src/component/ThemeToggleButton.jsx
@@ -1,19 +1,24 @@
 import { useTheme } from "../context/ThemeContext";
 import { useThemeDispatch } from "../context/ThemeContext";
+import { IoMdSunny } from "react-icons/io";
+import { MdNightsStay } from "react-icons/md";
 
-export default function ThemeToggleButton({ icon }) {
+export default function ThemeToggleButton() {
   const theme = useTheme();
   const themeDispatch = useThemeDispatch();
+
   const changeTheme = () => {
     themeDispatch({ type: theme === "light" ? "dark" : "light" });
   };
+
+  const Icon = theme === "light" ? MdNightsStay : IoMdSunny;
 
   return (
     <button
       className="flex items-center justify-center py-2 text-center border-black bg-purple-600 dark:bg-yellow-600 rounded-lg text-white w-16"
       onClick={changeTheme}
     >
-      {icon}
+      <Icon />
     </button>
   );
 }
