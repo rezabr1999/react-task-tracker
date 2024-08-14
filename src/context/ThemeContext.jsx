063aca7c9919212b7088eb11eb82db0f8aa2f1fd
Reza Br <reza.linusx@gmail.com>
2024-08-14 15:29:11 +0330

    store and get user dark or light mode preference

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,8 +5,14 @@ const themeContext = createContext();
 const themeDispatchContext = createContext();
 
 export default function ThemeProvider({ children }) {
-  
-  const [theme, themeDispatcher] = useReducer(ThemeReducer, "light");
+  const storedTheme = JSON.parse(localStorage.getItem("theme"));
+  const systemPrefersDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const initialTheme = storedTheme || (systemPrefersDark ? "dark" : "light");
+
+  const [theme, themeDispatcher] = useReducer(ThemeReducer, initialTheme);
   return (
     <themeContext.Provider value={theme}>
       <themeDispatchContext.Provider value={themeDispatcher}>
--- src/reducer/ThemeReducer.jsx
+++ src/reducer/ThemeReducer.jsx
@@ -1,9 +1,11 @@
 export function ThemeReducer(state, action) {
   switch (action.type) {
     case "dark":
+      localStorage.setItem("theme", JSON.stringify("dark"));
       return "dark";
 
     case "light":
+      localStorage.setItem("theme", JSON.stringify("light"));
       return "light";
 
     default:
