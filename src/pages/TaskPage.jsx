845df6a50b6362d2743be0beab414f820d3b6df0
Reza Br <reza.linusx@gmail.com>
2024-08-16 12:33:10 +0330

    add creation date to task page

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -6,11 +6,15 @@ export default function TaskPage() {
   const { id } = useParams();
   const task = tasks.find((task) => task.id === Number(id));
 
+  const creationDateStap = new Date(task.id);
+  const creationDate = creationDateStap.toDateString();
+
   return (
-    <div className="min-h-screen p-5">
-      <div className="max-w-xs mx-auto">
-        <h1 className="text-lg dark:text-white">{task.text}</h1>
-      </div>
+    <div className="min-h-screen w-full dark:text-white mt-5">
+      <h1 className="text-3xl pb-10 ">{task.text}</h1>
+      <p className="bg-slate-700 text-white mt-4 dark:bg-slate-200 dark:text-slate-700 inline-block p-3 rounded-sm w-52 text-center">
+        {creationDate}
+      </p>
     </div>
   );
 }
